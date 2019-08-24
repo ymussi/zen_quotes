96a62b995a28fa68c4357d2da3a7c9f96525d7fc
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2019-08-23 23:59:52 -0300

    change tag

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='zen_quotes',
     packages = ['quotes'],
-    version='1.0.1git ',
+    version='1.0.1',
     description='A sample lib Zen of Python',
     author='Yuri Mussi',
     author_email='ymussi@gmail.com',
