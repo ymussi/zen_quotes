665bae5ef08f4e1de1fb9ccf4fc62f7cbaf71faa
Yuri Mussi <mussi@MacBook-Air-de-Yuri.local>
2020-02-24 14:52:05 -0300

    upload description project

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name='zen_quotes',
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
     version = '1.1.0',
     setup_requires=["setuptools-git-version"],
     packages = find_packages(),
